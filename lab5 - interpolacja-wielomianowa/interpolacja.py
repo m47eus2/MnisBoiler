e0086dd5a39bd537ff84343c9b5fe73a1922b960
marhew2 <mateusz.ciesielski2003@gmail.com>
2024-06-01 17:03:03 +0200

    Adding lab5

--- lab5 - interpolacja-wielomianowa/interpolacja.py
+++ lab5 - interpolacja-wielomianowa/interpolacja.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-#współczynniki wielomianu obnliczone za pomocą różnic dzielonych
+# Obliczanie współczynników różnic dzielonych
 def divided_diff(x, y):
     n = len(y)
     coef = np.zeros([n, n])
@@ -12,8 +12,22 @@ def divided_diff(x, y):
             
     return coef[0, :]
 
-#podstawienie nowych wartości x do otrzymanego wielomianu
-def newton_poly(coef, x_data, x):
+# Funkcja do tworzenia pełnego wielomianu w postaci standardowej
+def newton_polynomial(coef, x_data):
+    n = len(coef)
+    poly = np.poly1d([0.0])
+    term = np.poly1d([1.0])
+    
+    for k in range(n):
+        term_k = np.poly1d([1.0])
+        for j in range(k):
+            term_k *= np.poly1d([1.0, -x_data[j]])
+        poly += coef[k] * term_k
+    
+    return poly
+
+# Funkcja do obliczania wartości wielomianu w punkcie x
+def evaluate_newton_poly(coef, x_data, x):
     n = len(coef)
     p = coef[0]
     for k in range(1, n):
@@ -26,6 +40,11 @@ y = np.array([1, 4, 9])
 coef = divided_diff(x, y)
 print("Współczynniki różnic dzielonych:", coef)
 
+# Generowanie wielomianu
+polynomial = newton_polynomial(coef, x)
+print("Współczynniki wielomianu:", polynomial.coefficients)
+
+# Obliczanie wartości interpolowanej
 x_new = 2.5
-y_new = newton_poly(coef, x, x_new)
+y_new = evaluate_newton_poly(coef, x, x_new)
 print(f"Wartość interpolowana w x = {x_new}: {y_new}")
